e62768ecb6389b6c27b9149a11e8d89aad584272
Rick Pastoor <rick@wrep.nl>
2013-02-28 14:57:31 +0100

    Check in basic bundle structure

---
+++ NotificareBundle.php
@@ -0,0 +1,15 @@
+<?php
+namespace Wrep\Bundle\NotificareBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * NotificareBundle class
+ *
+ * @author		Wrep <info@wrep.nl>
+ * @copyright	(c) 2013 Wrep
+ * @license		http://www.opensource.org/licenses/mit-license.php
+ */
+class NotificareBundle extends Bundle
+{
+
+}
