73d111d4699efedd4deb63577996f974efd0e7f5
Rick Pastoor <rick@wrep.nl>
2013-02-28 17:31:39 +0100

    Passing on cert

--- Notificare/Sender.php
+++ Notificare/Sender.php
@@ -2,6 +2,7 @@
 namespace Wrep\Bundle\NotificareBundle\Notificare;
 
 use Wrep\Notificare\Apns\Sender as NotificareSender;
+use Wrep\Notificate\Apns\Certificate;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Sender extends NotificareSender
@@ -10,6 +11,17 @@ class Sender extends NotificareSender
 
 	public function __construct(ContainerInterface $container)
 	{
+		// Store container
 		$this->container = $container;
+
+		// Create default certificate
+		$certificate = null;
+		if ($this->container->getParameter('notificare.apns.certificate.pem') != null)
+		{
+			$certificate = new Certificate($this->container->getParameter('notificare.apns.certificate.pem'), $this->container->getParameter('notificare.apns.certificate.passphrase'));
+		}
+
+		// Call our constructor, passing certificate or null
+		parent::__constructor($certificate);
 	}
 }
